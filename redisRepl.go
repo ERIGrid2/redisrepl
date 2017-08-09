1d59697ce0b581d166ae1c8df6f652887134a133
danielePala <pala.daniele@gmail.com>
2017-08-09 17:14:49 +0200

    start to give a more decent look to the code

--- redisRepl.go
+++ redisRepl.go
@@ -1,17 +1,17 @@
 /*
  Main redisRepl program.
 
- Copyright 2017 Daniele Pala <daniele.pala@rse-web.it>                                                                                                                                                             
+ Copyright 2017 Daniele Pala <daniele.pala@rse-web.it>
 
- This file is part of redisRepl. 
+ This file is part of redisRepl.
 
  redisRepl is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.                                                                                                                                                                               
+ (at your option) any later version.
 
  redisRepl is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
@@ -30,26 +30,66 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
-	"os"
 )
 
 const version = "master"
-var remKey, remVal string
-var remHashKey string
-var remHashVal map[string]interface{}
-// version flag
-var showVersion = flag.Bool("version", false, "Show version and exit")
-// redis flags
-var redisAddr = flag.String("raddr", ":6379", "Redis server address")
-var nam = flag.String("nam", "dummyRI", "RI namespace")
-var remCmds = flag.Bool("rc", false, "Enable remote commands (disabled by default)")
-// https flags
-var httpsAddr = flag.String("haddr", "https://localhost/redis", "HTTPS server address")
-var certFile = flag.String("cert", "someCertFile", "A PEM encoded certificate file.")
-var keyFile = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
-var caFile = flag.String("CA", "someCertCAFile", "A PEM encoded CA's certificate file.")
+
+// Replicator is a remote Redis replicator. It replicates every 'set' or 'hset'
+// command from a local Redis instance to a remote one, using the Webdis
+// protocol. Optionally, it can also work in the reverse direction.
+type Replicator struct {
+	httpsAddr        string                 // HTTPS server address
+	client           *http.Client           // HTTPS client
+	cloc, csub, crem redis.Conn             // Redis connections
+	remKey, remVal   string                 // last remote key-value
+	remHashKey       string                 // last remote hash key
+	remHashVal       map[string]interface{} // last remote hash value
+}
+
+// ReplOpt contains options to be used by a Replicator.
+type ReplOpt struct {
+	redisAddr string       // Local Redis server address
+	httpsAddr string       // HTTPS server address
+	client    *http.Client // HTTPS client
+	remCmds   bool         // enable remote -> local replication?
+	nam       string       // namespace to replicate
+}
+
+func main() {
+	// parse cmdline flags
+	// version flag
+	var showVersion = flag.Bool("version", false, "Show version and exit")
+	// security certificates
+	var certFile = flag.String("cert", "someCertFile", "A PEM encoded certificate file.")
+	var keyFile = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
+	var caFile = flag.String("CA", "someCertCAFile", "A PEM encoded CA's certificate file.")
+	// redis flags
+	var redisAddr = flag.String("raddr", ":6379", "Redis server address")
+	var remCmds = flag.Bool("rc", false, "Enable remote commands (disabled by default)")
+	var nam = flag.String("nam", "dummyRI", "RI namespace")
+	// https flags
+	var httpsAddr = flag.String("haddr", "https://localhost/redis", "HTTPS server address")
+	flag.Parse()
+	// build replicator options
+	client := https_client(*certFile, *keyFile, *caFile)
+	if *showVersion {
+		log.Printf("RedisRepl version: %s\n", version)
+		os.Exit(0)
+	}
+	opts := ReplOpt{
+		redisAddr: *redisAddr,
+		httpsAddr: *httpsAddr,
+		client:    client,
+		remCmds:   *remCmds,
+		nam:       *nam,
+	}
+	// start replicator
+	replicator := Replicator{}
+	replicator.Start(opts)
+}
 
 // Main functions:
 // 1. connect to local Redis via TCP and remote redis via HTTPS
@@ -59,40 +99,41 @@ var caFile = flag.String("CA", "someCertCAFile", "A PEM encoded CA's certificate
 //    begins with <nam> remotely via HTTPS
 // 4. optionally, replicate any remote 'set' command whose key
 //    begins with <nam> into the local Redis.
-func main() {
-	flag.Parse()
-	if *showVersion {
-		log.Printf("RedisRepl version: %s\n", version)
-		os.Exit(0)
-	}
+func (r *Replicator) Start(opts ReplOpt) {
+	r.httpsAddr = opts.httpsAddr
+	r.client = opts.client
 	// create a Redis connection pool
 	pool := redis.Pool{
 		MaxIdle: 3,
-		Dial: func () (redis.Conn, error) {
-			return redis.Dial("tcp", *redisAddr)
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", opts.redisAddr)
 		},
 	}
 	// connection to Redis, for local listener
-	cloc := pool.Get()
-	defer cloc.Close()
+	r.cloc = pool.Get()
+	defer r.cloc.Close()
 	// additional connection to Redis for pubsub
-	csub := pool.Get()
-	defer csub.Close()
-	client := https_client()
+	r.csub = pool.Get()
+	defer r.csub.Close()
 	// Enable key-event notifications for strings and hashes
-	cloc.Do("CONFIG", "set", "notify-keyspace-events", "K$h")
-	setupHttps(pool, client)
+	r.cloc.Do("CONFIG", "set", "notify-keyspace-events", "K$h")
+	// if needed, start listening for remote events
+	if opts.remCmds == true {
+		// another connection to Redis, for remote receiver
+		r.crem = pool.Get()
+		go r.receive(opts.nam)
+	}
 	// subscribe to events from local Redis instance
-	log.Printf("Replicating local Redis instance to remote address: %s\n", *httpsAddr)
-	psc := redis.PubSubConn{Conn: csub}
+	log.Printf("Replicating local Redis instance to remote address: %s\n", r.httpsAddr)
+	psc := redis.PubSubConn{Conn: r.csub}
 	psc.PSubscribe("__keyspace@0__:*")
 	for {
 		switch v := psc.Receive().(type) {
 		case redis.PMessage:
 			cmd := string(v.Data)
 			key := strings.TrimPrefix(v.Channel, "__keyspace@0__:")
-			if strings.HasPrefix(key, *nam) {
-				handleCmd(cmd, key, cloc, client)
+			if strings.HasPrefix(key, opts.nam) {
+				r.handleCmd(cmd, key)
 			}
 		case redis.Subscription:
 		case error:
@@ -102,40 +143,31 @@ func main() {
 	}
 }
 
-func setupHttps(pool redis.Pool, client *http.Client) {
-	// if needed, start listening for remote events
-	if *remCmds == true {
-		// another connection to Redis, for remote receiver
-		crem := pool.Get()
-		go receive(client, crem)
-	}
-}
-
-func handleCmd(cmd, key string, c redis.Conn, client *http.Client) {
+func (r *Replicator) handleCmd(cmd, key string) {
 	switch cmd {
-	case "set":	
-		val, e := redis.String(c.Do("GET", key))
+	case "set":
+		val, e := redis.String(r.cloc.Do("GET", key))
 		if e != nil {
 			log.Printf("Error reading %s from Redis\n", key)
 		}
-		if key != remKey || val != remVal {
+		if key != r.remKey || val != r.remVal {
 			redisData := "SET/" + escape(key) + "/" + escape(val)
 			log.Printf("(local)->(remote) %s %s\n", key, val)
-			send(*httpsAddr, redisData, client)
+			send(r.httpsAddr, redisData, r.client)
 		}
 	case "hset":
-		fields, e := redis.StringMap(c.Do("HGETALL", key))
+		fields, e := redis.StringMap(r.cloc.Do("HGETALL", key))
 		if e != nil {
 			log.Printf("Error reading %s from Redis\n", key)
 		}
-		if key != remHashKey || hashCmp(c, key, remHashVal) == false {
+		if key != r.remHashKey || hashCmp(r.cloc, key, r.remHashVal) == false {
 			redisData := "HMSET/" + key
 			for k, v := range fields {
 				redisData = redisData + "/" + escape(k)
 				redisData = redisData + "/" + escape(v)
 			}
 			log.Printf("(local)->(remote) hash %s\n", key)
-			send(*httpsAddr, redisData, client)
+			send(r.httpsAddr, redisData, r.client)
 		}
 	default:
 		log.Printf("(local) Unhandled event: %s\n", cmd)
@@ -143,15 +175,15 @@ func handleCmd(cmd, key string, c redis.Conn, client *http.Client) {
 }
 
 // Create a new HTTPS client with the given certificate files.
-func https_client() *http.Client {
+func https_client(certFile, keyFile, caFile string) *http.Client {
 	// Load client cert
-	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Load CA cert
-	caCert, err := ioutil.ReadFile(*caFile)
+	caCert, err := ioutil.ReadFile(caFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -166,7 +198,6 @@ func https_client() *http.Client {
 	tlsConfig.BuildNameToCertificate()
 	transport := &http.Transport{
 		TLSClientConfig: tlsConfig,
-
 	}
 	client := &http.Client{
 		Transport: transport,
@@ -175,7 +206,7 @@ func https_client() *http.Client {
 }
 
 // Send a new command to remote Redis via HTTPS. The command uses the Webdis
-// format. 
+// format.
 func send(addr, cmd string, c *http.Client) (string, interface{}) {
 	// send the actual command
 	resp, e := c.Post(addr+"/", "text/plain", strings.NewReader(cmd))
@@ -187,7 +218,7 @@ func send(addr, cmd string, c *http.Client) (string, interface{}) {
 	// return response parsed from JSON
 	k, v := parseSingle(json.NewDecoder(resp.Body))
 	ioutil.ReadAll(resp.Body)
-	return k, v;
+	return k, v
 }
 
 // Parse a JSON message, assuming it has a single field (like a reply after
@@ -207,16 +238,16 @@ func parseSingle(dec *json.Decoder) (string, interface{}) {
 }
 
 // Receive and handle a message from the remote subscription.
-func receive(client *http.Client, c redis.Conn) {
-	log.Printf("Subscribing for remote changes to the namespace: %s\n", *nam)
+func (r *Replicator) receive(nam string) {
+	log.Printf("Subscribing for remote changes to the namespace: %s\n", nam)
 	// Enable key-event notifications for strings and hashes
-	send(*httpsAddr, "CONFIG/set/notify-keyspace-events/K$h", client)
+	send(r.httpsAddr, "CONFIG/set/notify-keyspace-events/K$h", r.client)
 
-	resp, e := client.Get(*httpsAddr + "/PSUBSCRIBE/__keyspace@0__:" + *nam + "*")
+	resp, e := r.client.Get(r.httpsAddr + "/PSUBSCRIBE/__keyspace@0__:" + nam + "*")
 	if e != nil {
 		log.Println(e)
 		time.Sleep(10 * time.Second)
-		go receive(client, c)
+		go r.receive(nam)
 		return
 	}
 	defer resp.Body.Close()
@@ -231,17 +262,17 @@ func receive(client *http.Client, c redis.Conn) {
 		for k, v := range m {
 			switch k {
 			case "PSUBSCRIBE":
-				sub(client, v, c)
+				r.sub(v)
 			}
 		}
 	}
 	// TODO: retry connection
 	log.Println("Error: remote HTTPS connection down, retrying in 10 seconds")
 	time.Sleep(10 * time.Second)
-	go receive(client, c)
+	go r.receive(nam)
 }
 
-func sub(client *http.Client, v interface{}, c redis.Conn) {
+func (r *Replicator) sub(v interface{}) {
 	redisPayload := v.([]interface{})
 	msgType := redisPayload[0].(string)
 	if msgType == "pmessage" {
@@ -250,9 +281,9 @@ func sub(client *http.Client, v interface{}, c redis.Conn) {
 		key := strings.TrimPrefix(keySpace, "__keyspace@0__:")
 		switch cmd {
 		case "set":
-			_, res := send(*httpsAddr, "GET/" + escape(key), client)
+			_, res := send(r.httpsAddr, "GET/"+escape(key), r.client)
 			val := res.(string)
-			actVal, e := redis.String(c.Do("GET", key))
+			actVal, e := redis.String(r.crem.Do("GET", key))
 			if e != nil {
 				log.Printf("Reading %s from Redis: %s\n", key, e)
 			}
@@ -260,38 +291,38 @@ func sub(client *http.Client, v interface{}, c redis.Conn) {
 			// already stored.
 			if actVal != val {
 				log.Printf("(remote)->(local) %s %s\n", key, val)
-				set(key, val, c)
+				r.set(key, val)
 			}
 		case "hset":
-			_, res := send(*httpsAddr, "HGETALL/" + escape(key), client)
+			_, res := send(r.httpsAddr, "HGETALL/"+escape(key), r.client)
 			val := res.(map[string]interface{})
 			// write the value only if it is different from the one
-			// already stored. 
-			if hashCmp(c, key, val) == false {
+			// already stored.
+			if hashCmp(r.crem, key, val) == false {
 				log.Printf("(remote)->(local) %s %v\n", key, val)
-				hset(key, val, c)
+				r.hset(key, val)
 			}
 		}
 	}
 }
 
-func set(key string, val string, c redis.Conn) {
-	c.Do("SET", key, val)
-	remKey = key
-	remVal = val
+func (r *Replicator) set(key string, val string) {
+	r.crem.Do("SET", key, val)
+	r.remKey = key
+	r.remVal = val
 }
 
-func hset(key string, val map[string]interface{}, c redis.Conn) {
+func (r *Replicator) hset(key string, val map[string]interface{}) {
 	args := []interface{}{key}
 	for hk, hv := range val {
 		args = append(args, hk, hv)
 	}
-	_, e := c.Do("HMSET", args...)
+	_, e := r.crem.Do("HMSET", args...)
 	if e != nil {
 		log.Println(e)
 	}
-	remHashKey = key
-	remHashVal = val
+	r.remHashKey = key
+	r.remHashVal = val
 }
 
 func escape(msg string) string {
--- redisRepl_test.go
+++ redisRepl_test.go
@@ -1,17 +1,17 @@
 /*
  redisRepl tests.
 
- Copyright 2017 Daniele Pala <daniele.pala@rse-web.it>                                                                                                                                                             
+ Copyright 2017 Daniele Pala <daniele.pala@rse-web.it>
 
- This file is part of redisRepl. 
+ This file is part of redisRepl.
 
  redisRepl is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.                                                                                                                                                                               
+ (at your option) any later version.
 
  redisRepl is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
@@ -22,8 +22,8 @@
 package main
 
 import (
-	"testing"
 	redis "github.com/garyburd/redigo/redis"
+	"testing"
 )
 
 // Test 1
@@ -32,7 +32,7 @@ import (
 func TestHashCmp1(t *testing.T) {
 	c := redisConn()
 	hashName := "test1"
-	hash := map[string]interface{} {
+	hash := map[string]interface{}{
 		"one": "1",
 		"two": "2",
 	}
@@ -51,26 +51,30 @@ func TestHashCmp1(t *testing.T) {
 func TestHashCmp2(t *testing.T) {
 	c := redisConn()
 	hashName := "test1"
-	hash := map[string]interface{} {
+	hash := map[string]interface{}{
 		"one": "1",
 		"two": "2",
 	}
 	for hk, hv := range hash {
 		c.Do("HSET", hashName, hk, hv)
 	}
-	hash["three"] = "3";
+	hash["three"] = "3"
 	res := hashCmp(c, hashName, hash)
 	if res == true {
 		t.FailNow()
 	}
 }
 
+/*func TestMain(t *testing.T) {
+	main()
+}*/
+
 func redisConn() redis.Conn {
 	var redisAddr = ":6379"
 	// create a Redis connection pool
 	pool := redis.Pool{
 		MaxIdle: 3,
-		Dial: func () (redis.Conn, error) {
+		Dial: func() (redis.Conn, error) {
 			return redis.Dial("tcp", redisAddr)
 		},
 	}
